74e573d819f7cfb7fd8e98ea95dcf0fc458423e1
Pappo Khan <110984853+Pappokhan@users.noreply.github.com>
2023-07-07 18:15:35 +0600

    Python Periodic table.py

--- Python Periodic table.py
+++ Python Periodic table.py
@@ -4,28 +4,27 @@ def Periodic_table():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
 
-    if username == "yes" and password == "ok":
-        print()
-        print("       Login successful!   ")
-        print("--Wellcome to Periodic-table!--")
-        print()
-
-        Atomic_Number = int(input("Enter your Atomic Number: "))
-        element = periodictable.elements[Atomic_Number]
-        
-        print("--------------------------------")
-        print("|  Atomic Symbol  :", element.symbol, "        |")
-        print("|  Atomic Name    :", element.name, "  |")
-        print("|  Atomic mass    :", element.mass, "    |")
-        print("|  Atomic Density :", element.density, "      |")
-        print("--------------------------------")
-
-        while True:
+    while True:
+        if username == "yea" and password == "ok":
+            print()
+            print("      Login successful!   ")
+            print("--Wellcome to Periodictable!--")
+            print()
+            
+            Atomic_Number = int(input("Enter your Atomic Number: "))
+            element = periodictable.elements[Atomic_Number]
+            
+            print("--------------------------------")
+            print("|  Atomic Symbol  :", element.symbol)
+            print("|  Atomic Name    :", element.name,)
+            print("|  Atomic mass    :", element.mass,)
+            print("|  Atomic Density :", element.density,)
+            print("--------------------------------")
+            
             next_calculation = input("You want to see more Atomic information? yes/no: ")
             if next_calculation == "no":
                 break
-    else:
-        print("Invalid ID...")
+            else:
+                print("Invalid ID...")
 
 Periodic_table()
-
